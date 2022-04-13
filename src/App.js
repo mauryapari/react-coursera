419381aaee006de4cf0cc35c2c3ae51c67a4859e
Paritosh Maurya <paritosh.maurya@tadigital.com>
2022-04-13 15:11:46 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
+import { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-   <div className="App">
-		<Navbar dark color="primary">
-			<div className="container">
-				<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+	constructor(props) {
+	  super(props);
+	  
+	  this.state = {
+		 dishes: DISHES
+	  };
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Navbar dark color="primary">
+					<div className="container">
+						<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+					</div>
+				</Navbar>
+				<Menu dishes={this.state.dishes}/>
 			</div>
-		</Navbar>
-		<Menu />
-	</div>
-  );
+		);
+	}
 }
 
 export default App;
