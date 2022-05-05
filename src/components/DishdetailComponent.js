be217fa17150f36372d9357b8401224b0b4940ac
Paritosh Maurya <paritosh.maurya@tadigital.com>
2022-05-05 18:27:46 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -20,9 +20,10 @@ class CommentForm extends Component {
    }
 
    handleSubmit(values) {
-      console.log(values);
-      this.toggleModal();
       // code to deal with when form is submitted.
+      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+      this.toggleModal();
+
    }
 
    toggleModal() {
@@ -110,7 +111,7 @@ function RenderDish( {dish} )  {
    return (<div></div>);
 }
 
-function RenderComments( {comments }) {
+function RenderComments( {comments, addComment, dishId}) {
    if(comments && comments.length) {
       const list = comments.map(item=> {
          return (
@@ -126,7 +127,7 @@ function RenderComments( {comments }) {
             <ul className="list-unstyled">
                { list }
             </ul>
-            <CommentForm></CommentForm>
+            <CommentForm dishId={dishId} addComment={addComment}></CommentForm>
          </div>
       )
    }
@@ -150,7 +151,10 @@ const DishDetail = (props) => {
                <RenderDish dish={props.dish} />
             </div>
             <div className="col-12 col-md-5 m-1">
-               <RenderComments comments = {props?.comments} />
+               <RenderComments comments = {props?.comments} 
+                  addComment={props.addComment}
+                  dishId={props.dish.id}
+               />
             </div>
          </div>
       </div>
